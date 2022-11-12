90bcfe6e720ec3536a5d7d492a758b99dd5929bf
Henry Rovnyak <h.rovnyak@proton.me>
2022-11-11 20:25:23 -0500

    Make letters colliding with soup work using mesh data

--- objects/soup/soup.gd
+++ objects/soup/soup.gd
@@ -1,10 +1,13 @@
 extends MeshInstance
 
-export var height: float = 0
 export var buoyancy_acceleration = 9.81 * 2
-export var drag = 2
+var drag = 2
+
+var mesh_points = self.mesh.get_faces()
 
 var letters = []
+
+var triangles = []
 
 # Recursively search the node tree for all the letters and push them to the list
 func push_letters(node: Node):
@@ -17,12 +20,25 @@ func push_letters(node: Node):
 		push_letters(child)
 
 
+# Get all of the triangles in the mesh, change the data structure to be easier to work with, and flatten them into 2d triangles
+func get_triangles():
+	
+	for i in range(0, (mesh_points.size() / 3)):
+		
+		var p1 = mesh_points[i * 3 + 0]
+		var p2 = mesh_points[i * 3 + 1]
+		var p3 = mesh_points[i * 3 + 2]
+		
+		triangles.push_back([Vector2(p1.x, p1.z), Vector2(p2.x, p2.y), Vector2(p3.x, p3.y)])
+
 func _ready():
 	push_letters(get_tree().get_root())
+	
+	get_triangles()
 
 
 func buoyancy(letter_height: float):
-	var depth = height - letter_height
+	var depth = self.translation.y - letter_height
 	
 	if depth < -0.1:
 		return 0
@@ -37,13 +53,20 @@ func _process(delta):
 		var pos = letter.translation
 		
 		# Check if the letter is below the soup, then check if it's in the soup
-		if pos.y < height + 0.1: #&& Geometry.is_point_in_polygon(Vector2(pos.x, pos.z), points):
-			var acceleration = buoyancy(pos.y)
+		if pos.y < self.translation.y + 0.1:
 			
-			# Buoyancy
-			letter.apply_central_impulse(Vector3(0, acceleration, 0) * delta * letter.mass)
-			
-			# Drag
-			letter.apply_central_impulse(letter.linear_velocity * drag * delta * -1)
+			# Check if the letter is above or below one of the triangles in the mesh
+			for triangle in triangles:
+				
+				if Geometry.is_point_in_polygon(Vector2(pos.x, pos.z), triangle):
+					var acceleration = buoyancy(pos.y)
+					
+					# Buoyancy
+					letter.apply_central_impulse(Vector3(0, acceleration, 0) * delta * letter.mass)
+					
+					# Drag
+					letter.apply_central_impulse(letter.linear_velocity * drag * delta * -1)
+					
+					break
 	
 	pass
