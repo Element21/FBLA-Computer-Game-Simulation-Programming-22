e3da61f2d44c31ed717a730f7a8d3f45a2c71062
Henry Rovnyak <h.rovnyak@proton.me>
2022-11-13 16:05:50 -0500

    Hand tracking works

--- objects/hand/hand.gd
+++ objects/hand/hand.gd
@@ -5,26 +5,27 @@ extends Node
 export(NodePath) onready var camera = get_node(camera) as Camera
 
 var raycast: RayCast
+var hand: MeshInstance
 
 
 func _ready():
 	
-	raycast = get_child(0)
+	hand = get_child(0)
+	raycast = get_child(1)
 
 
 func _process(delta):
 	
 	var mouse_pos = get_viewport().get_mouse_position()
 	
-	var from = camera.project_ray_origin(mouse_pos) - self.translation
-	var to = camera.project_ray_normal(mouse_pos) * 1000 - self.translation
-
+	var from = camera.project_ray_origin(mouse_pos)
+	var to = camera.project_ray_normal(mouse_pos) * 1000
+	
 	raycast.cast_to = to
 	raycast.translation = from
 
 	if raycast.is_colliding():
 		
-		print(raycast.get_collider(), raycast.get_collision_point())
-		self.translation = raycast.get_collision_point() + Vector3(0, 1, 0)
+		hand.translation = raycast.get_collision_point() + Vector3(0, 2, 0)
 	
 	pass
