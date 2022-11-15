3a72e5f675be7b75adc3bf4332b71c0534c0f272
Henry Rovnyak <h.rovnyak@proton.me>
2022-11-14 19:21:55 -0500

    Make the arm not perfectly responsive

--- objects/hand/hand.gd
+++ objects/hand/hand.gd
@@ -1,34 +1,36 @@
 # Soup must be on collision layer 2
 
-extends Node
+extends Spatial
 
 export(NodePath) onready var camera = get_node(camera) as Camera
 export(Vector2) var arm_pivot = Vector2(-1, -1)
 
-var raycast: RayCast
-var hand: MeshInstance
+var inverse_arm_response = 0.5
 
+onready var hand: MeshInstance = get_child(0)
+onready var raycast: RayCast = get_child(1)
 
-func _ready():
-	
-	hand = get_child(0)
-	raycast = get_child(1)
+onready var target_pos: Vector3 = hand.translation
 
 
 func _process(delta):
+	
+	hand.translation = lerp(hand.translation, target_pos, pow(delta, inverse_arm_response))
+	
+	hand.rotation.y = -Vector2(hand.translation.x, hand.translation.z).angle_to_point(arm_pivot) - PI / 2
 	
 	var mouse_pos = get_viewport().get_mouse_position()
 	
 	var from = camera.project_ray_origin(mouse_pos)
 	var to = camera.project_ray_normal(mouse_pos) * 1000
 	
-	raycast.cast_to = to
-	raycast.translation = from
+	raycast.cast_to = to - self.translation
+	raycast.translation = from - self.translation
 
 	if raycast.is_colliding():
 		
-		hand.translation = raycast.get_collision_point() + Vector3(0, 2, 0)
+		target_pos = raycast.get_collision_point() + Vector3(0, 2, 0) - self.translation
 		
-		hand.rotation.y = -Vector2(hand.translation.x, hand.translation.z).angle_to_point(arm_pivot) - PI / 2
+
 	
 	pass
