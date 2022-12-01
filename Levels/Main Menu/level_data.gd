9eec149ae887b332a830e6f2efb71321bcefda08
Henry Rovnyak <h.rovnyak@proton.me>
2022-12-01 16:26:04 -0500

    Leaderboard

--- Levels/Main Menu/level_data.gd
+++ Levels/Main Menu/level_data.gd
@@ -3,12 +3,18 @@ extends Control
 class_name LevelData
 
 
+var max_leaderboard_entries = 10
+var leaderboard_entry_horizontal_spacing = 10
+var leaderboard_entry_vertical_separation = 30
+
+
 var level_idx = null
 var level_scene = null
 
 onready var level_label: Label = get_child(0)
 onready var highscore_label: Label = get_child(1)
 onready var play_button: Button = get_child(2)
+onready var leaderboard: Control = get_child(3)
 
 
 func _ready():
@@ -29,6 +35,25 @@ func select_level(idx: int, name: String, scene: PackedScene):
 		highscore_label.text = ""
 	else:
 		highscore_label.text = "Your highscore: " + String(highscore)
+	
+	var leaderboard_entries = LeaderboardManager.leaderboard_data.get_sorted_leaderboard_for(level_idx)
+	
+	for i in range(0, min(max_leaderboard_entries, leaderboard_entries.size())):
+		var name_label = Label.new()
+		name_label.align = Label.ALIGN_RIGHT
+		name_label.grow_horizontal = Control.GROW_DIRECTION_BEGIN
+		name_label.text = leaderboard_entries[i][0]
+		name_label.rect_position.x = -leaderboard_entry_horizontal_spacing / 2
+		name_label.rect_position.y = leaderboard_entry_vertical_separation * i
+		leaderboard.add_child(name_label)
+		
+		var score_label = Label.new()
+		score_label.align = Label.ALIGN_LEFT
+		score_label.align = Control.GROW_DIRECTION_END
+		score_label.text = String(leaderboard_entries[i][1])
+		score_label.rect_position.x = leaderboard_entry_horizontal_spacing / 2
+		score_label.rect_position.y = leaderboard_entry_vertical_separation * i
+		leaderboard.add_child(score_label)
 	
 	self.show()
 
--- objects/leaderboard_data.gd
+++ objects/leaderboard_data.gd
@@ -4,4 +4,25 @@ class_name LeaderboardData
 
 # Array, each entry contains leaderboard data for the level with the same index (first value contains data for level 1)
 # Each entry is a dictionary from `name` to `highest score`
-export var data: Array = [{}]
+export var data: Array = [{
+	test = 5,
+	bruh = 9,
+}]
+
+
+class LeaderboardDataSorter:
+	static func sort_ascending(a, b):
+		return a[0] < b[0]
+
+
+func get_sorted_leaderboard_for(idx: int) -> Array:
+	var data_dictionary: Dictionary = data[idx]
+	
+	var data_array = []
+	
+	for key in data_dictionary.keys():
+		data_array.push_back([key, data_dictionary[key]])
+	
+	data_array.sort_custom(LeaderboardDataSorter, "sort_ascending")
+	
+	return data_array
