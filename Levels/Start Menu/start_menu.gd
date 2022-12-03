32d4894c77b5e35c753bfdbcaff3f2326d50cc1a
Henry Rovnyak <h.rovnyak@proton.me>
2022-12-03 11:47:32 -0500

    whoops

--- Levels/Start Menu/start_menu.gd
+++ Levels/Start Menu/start_menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 var main_menu_scene: PackedScene = preload("res://Levels/Main Menu/main_menu.tscn")
 
-onready var name_input = get_child(0)
+export(NodePath) onready var name_input = get_node(name_input) as LineEdit
 
 
 func go_to_main_menu(_from_signal):
