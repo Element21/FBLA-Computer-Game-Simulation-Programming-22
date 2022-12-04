79d8f4363a47c150f5f69cc82b1a175d74af4783
Henry Rovnyak <h.rovnyak@proton.me>
2022-12-04 09:40:39 -0500

    Apparently buttons can be clicked even if they're invisible

--- objects/level_end_screen/level_end_screen.gd
+++ objects/level_end_screen/level_end_screen.gd
@@ -5,20 +5,26 @@ class_name LevelEndScreen
 
 export(PackedScene) var next_level
 
+export(NodePath) onready var main_menu_button = get_node(main_menu_button) as Button
 export(NodePath) onready var next_level_button = get_node(next_level_button) as Button
 export(NodePath) onready var score_label = get_node(score_label) as Label
 
 func _ready():
 	self.hide()
 	
-	if !next_level:
-		next_level_button.hide()
+	# Prevent buttons from being clicked on (even though they're hidden by the root thing, idk)
+	main_menu_button.hide()
+	next_level_button.hide()
 
 
 func level_ended(score: int):
 	score_label.text = String(score)
 	
 	self.show()
+	main_menu_button.show()
+	
+	if next_level:
+		next_level_button.show()
 
 
 func main_menu():
