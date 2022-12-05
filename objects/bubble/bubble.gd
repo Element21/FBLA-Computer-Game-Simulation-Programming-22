6070de95dc816681433bf6cbc0e738ee708fef7e
Henry Rovnyak <h.rovnyak@proton.me>
2022-12-05 18:01:02 -0500

    Bubble pop effects

--- objects/bubble/bubble.gd
+++ objects/bubble/bubble.gd
@@ -6,6 +6,8 @@ var level: Level
 
 onready var bubble_mesh = get_node("Bubble mesh")
 onready var pop_sound = get_node("Pop sound")
+onready var particles = get_node("Particles")
+onready var delete_timer = get_node("Delete timer")
 
 var probability_of_pop_in_one_second = 0.5
 
@@ -18,8 +20,12 @@ func maybe_pop(delta: float):
 	if is_instance_valid(bubble_mesh) && randf() < probability_of_pop_in_one_second * delta:
 		level.bubbles_in_play.remove(level.bubbles_in_play.find(self))
 		bubble_mesh.queue_free()
+		
 		pop_sound.play()
+		particles.emitting = true
+		
+		delete_timer.start()
 
 
-func finished_playing_sound():
+func delete():
 	self.queue_free()
