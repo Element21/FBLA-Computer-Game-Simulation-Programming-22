c6f8f2f8f6f14c219318c15fda36e2e94f35bbe5
Henry Rovnyak <h.rovnyak@proton.me>
2022-12-05 15:54:08 -0500

    Bubble popping sound

--- objects/bubble/bubble.gd
+++ objects/bubble/bubble.gd
@@ -4,6 +4,9 @@ extends RigidBody
 # This is assigned by BubbleSpawner (WHY ARE NODE PATHS RELATIVE AAAAAAAAA)
 var level: Level
 
+onready var bubble_mesh = get_node("Bubble mesh")
+onready var pop_sound = get_node("Pop sound")
+
 var surface = 8.73
 var half_life = 1
 
@@ -13,9 +16,14 @@ func _ready():
 
 
 func _process(delta):
-	if self.global_translation.y > surface:
+	if is_instance_valid(bubble_mesh) && self.global_translation.y > surface:
 		self.global_translation.y = surface
 		
 		if randf() < 1 - pow((1 / half_life) * 0.5, delta):
 			level.bubbles_in_play.remove(level.bubbles_in_play.find(self))
-			self.queue_free()
+			bubble_mesh.queue_free()
+			pop_sound.play()
+
+
+func finished_playing_sound():
+	self.queue_free()
