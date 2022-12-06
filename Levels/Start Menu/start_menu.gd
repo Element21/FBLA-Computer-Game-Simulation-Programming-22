536cb440986f4ef48b3eb72c128a2922652df9ee
Henry Rovnyak <h.rovnyak@proton.me>
2022-12-06 15:43:19 -0500

    Make entering an empty name cause an error

--- Levels/Start Menu/start_menu.gd
+++ Levels/Start Menu/start_menu.gd
@@ -5,12 +5,19 @@ var main_menu_scene: PackedScene = preload("res://Levels/Main Menu/main_menu.tsc
 
 export(NodePath) onready var name_input = get_node(name_input) as LineEdit
 
+onready var empty_name_error = get_node("VBoxContainer/Empty name error")
+
 
 func go_to_main_menu(_from_signal):
-	var status = get_tree().change_scene_to(main_menu_scene)
-	assert(status == OK)
+	var name = name_input.text
 	
-	LeaderboardManager.set_player_name(name_input.text)
+	if name == "":
+		empty_name_error.visible_characters = -1
+	else:
+		LeaderboardManager.set_player_name(name)
+		
+		var status = get_tree().change_scene_to(main_menu_scene)
+		assert(status == OK)
 
 
 func quit():
