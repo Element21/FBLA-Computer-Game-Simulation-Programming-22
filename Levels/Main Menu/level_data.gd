7a3c72e79324350f7cb243453ad0fca6d4cf4e40
Henry Rovnyak <h.rovnyak@proton.me>
2022-12-12 16:56:54 -0500

    Make the leaderboard its own scene

--- Levels/Main Menu/level_data.gd
+++ Levels/Main Menu/level_data.gd
@@ -9,17 +9,13 @@ var max_leaderboard_entries = 10
 var level_idx = null
 var level_scene = null
 
-export(NodePath) onready var level_label = get_node(level_label) as Label
-export(NodePath) onready var highscore_label = get_node(highscore_label) as Label
-export(NodePath) onready var play_button = get_node(play_button) as Button
-export(NodePath) onready var leaderboard_names = get_node(leaderboard_names) as VBoxContainer
-export(NodePath) onready var leaderboard_scores = get_node(leaderboard_scores) as VBoxContainer
+onready var level_label = get_node("Level label") as Label
+onready var highscore_label = get_node("Highscore") as Label
+onready var leaderboard = get_node("Leaderboard") as Leaderboard
 
 
 func _ready():
 	self.hide()
-	
-	play_button.connect("button_down", self, "play")
 
 
 func select_level(level_idx: int, name: String, scene: PackedScene):
@@ -34,21 +30,7 @@ func select_level(level_idx: int, name: String, scene: PackedScene):
 	else:
 		highscore_label.text = "Your highscore: " + String(highscore)
 	
-	var leaderboard_entries = LeaderboardManager.leaderboard_data.get_sorted_leaderboard_for(level_idx)
-	
-	remove_children(leaderboard_names)
-	remove_children(leaderboard_scores)
-	
-	for i in range(0, min(max_leaderboard_entries, leaderboard_entries.size())):
-		var name_label = Label.new()
-		name_label.text = leaderboard_entries[i][0]
-		leaderboard_names.add_child(name_label)
-		
-		var score_label = Label.new()
-		score_label.text = String(leaderboard_entries[i][1])
-		leaderboard_scores.add_child(score_label)
-	
-	play_button.disabled = !LeaderboardManager.level_unlocked(level_idx)
+	leaderboard.show_leaderboard_for(level_idx)
 	
 	self.show()
 
@@ -59,9 +41,3 @@ func play():
 	Music.end_ambience()
 	
 	assert(status == OK)
-
-
-func remove_children(node: Node):
-	for child in node.get_children():
-		node.remove_child(child)
-		child.queue_free()
---
+++ objects/leaderboard/leaderboard.gd
@@ -0,0 +1,32 @@
+extends Control
+
+class_name Leaderboard
+
+
+export(int) var max_entries = 10
+
+
+export(NodePath) onready var names = get_node(names) as VBoxContainer
+export(NodePath) onready var scores = get_node(scores) as VBoxContainer
+
+
+func show_leaderboard_for(level_index: int):
+	var leaderboard_entries = LeaderboardManager.leaderboard_data.get_sorted_leaderboard_for(level_index)
+	
+	remove_children(names)
+	remove_children(scores)
+	
+	for i in range(0, min(max_entries, leaderboard_entries.size())):
+		var name_label = Label.new()
+		name_label.text = leaderboard_entries[i][0]
+		names.add_child(name_label)
+		
+		var score_label = Label.new()
+		score_label.text = String(leaderboard_entries[i][1])
+		scores.add_child(score_label)
+
+
+func remove_children(node: Node):
+	for child in node.get_children():
+		node.remove_child(child)
+		child.queue_free()
