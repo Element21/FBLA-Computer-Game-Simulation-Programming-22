6b58fff31681b450a8ecc6105a30a7d9c058e188
Henry Rovnyak <h.rovnyak@proton.me>
2022-12-12 19:47:16 -0500

    Fix buttons & compile document

--- objects/bottom_buttons/bottom_buttons.gd
+++ objects/bottom_buttons/bottom_buttons.gd
@@ -6,4 +6,5 @@ func quit():
 
 
 func documentation():
+	print(ProjectSettings.globalize_path("res://documentation/document.pdf"))
 	OS.shell_open(ProjectSettings.globalize_path("res://documentation/document.pdf"))
