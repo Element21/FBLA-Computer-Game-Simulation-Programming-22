45e80d11b54696d07af8d9d419d630e4a13301f3
Henry Rovnyak <h.rovnyak@proton.me>
2022-12-13 15:55:10 -0500

    Improve letter spawner

--- objects/letter_spawner/letter_spawner.gd
+++ objects/letter_spawner/letter_spawner.gd
@@ -1,29 +1,36 @@
 extends Spatial
 
-export var spawn_point: Vector3 = Vector3(0, 25, 0)
-export var initial_velocity: Vector3 = Vector3(0, 0, 0)
+export var upward_velocity: float = 0
+export var horizontal_velocity: float = 0
 export var spawn_amnt: int = 10
+export var spawn_interval: float = 3.0
 export(NodePath) var level
+
+onready var timer = $Timer
 
 onready var packed_scene = preload("res://objects/letter/letter.tscn")
 
 
-func spawn_letters(sp: Vector3=spawn_point, iv: Vector3=initial_velocity, sa: int = spawn_amnt):
-	for _i in range(sa):
+func spawn_letters():
+	for _i in range(spawn_amnt):
 		var letter = packed_scene.instance()
 		add_child(letter)
 		letter.add_to_group("Letter")
-		letter.global_transform.origin = sp
-		letter.apply_central_impulse(iv)
+		
+		letter.apply_central_impulse(Vector3(0, upward_velocity, 0))
+		
+		var random_angle = randf() * PI * 2
+		letter.apply_central_impulse(Vector3(cos(random_angle), 0, sin(random_angle)) * horizontal_velocity)
 
 
 func _ready():
 	get_node(level).connect("level_ended", self, "_on_level_ended")
+	timer.wait_time = spawn_interval
 
 
 func _on_level_ended():
 	print("level ended")
-	$"Timer".stop()
+	timer.stop()
 
 
 func _on_Timer_timeout():
