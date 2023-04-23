edbc632785f0d8f6dc7288c721bd2f98b772954c
Henry Rovnyak <h.rovnyak@proton.me>
2023-04-23 12:29:09 -0400

    Make the transition work when spammed

--- resources/shaders/transition.gd
+++ resources/shaders/transition.gd
@@ -7,19 +7,27 @@ func _ready():
 	@warning_ignore("return_value_discarded")
 	fade_in()
 
- 
-func fade_in() -> Signal:
+var current_tween = null
+
+func _fade_to(cutoff: float) -> Signal:
+	var initial_value = (self.material as ShaderMaterial).get_shader_parameter("cutoff")
+	
+	if current_tween != null && current_tween.is_running():
+		current_tween.stop()
+	
 	var tween = get_tree().create_tween()
 	@warning_ignore("return_value_discarded")
-	tween.tween_method(func(v): (self.material as ShaderMaterial).set_shader_parameter("cutoff", v), 0., 1., .3).set_trans(Tween.TRANS_SINE)
+	tween.tween_method(func(v): (self.material as ShaderMaterial).set_shader_parameter("cutoff", v), initial_value, cutoff, .3).set_trans(Tween.TRANS_SINE)
+	current_tween = tween
 	return tween.finished
+ 
+
+func fade_in() -> Signal:
+	return _fade_to(1.)
 
 
 func fade_out() -> Signal:
-	var tween = get_tree().create_tween()
-	@warning_ignore("return_value_discarded")
-	tween.tween_method(func(v): (self.material as ShaderMaterial).set_shader_parameter("cutoff", v), 1., 0., .3).set_trans(Tween.TRANS_SINE)
-	return tween.finished
+	return _fade_to(0.)
 
 
 func change_scene(scene: PackedScene):
